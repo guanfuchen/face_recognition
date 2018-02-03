e0af3c64915d44597897396ee83ed44d3710139d
guanfuchen <guanfuchen@zju.edu.cn>
2018-02-03 22:07:18 +0800

    增加预处理和训练人脸识别

---
+++ doc/develop_ideas.md
@@ -0,0 +1,5 @@
+# 开发相关问题
+
+主要参考如下链接：
+- [Building a Facial Recognition Pipeline with Deep Learning in Tensorflow](https://hackernoon.com/building-a-facial-recognition-pipeline-with-deep-learning-in-tensorflow-66e7645015b8)
+- [使用OpenFace进行人脸识别](http://blog.topspeedsnail.com/archives/10933)
--- face_recognition/dataloader/vggface_loader.py
+++ face_recognition/dataloader/vggface_loader.py
@@ -13,7 +13,7 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
-class lfwInfo:
+class vggfaceInfo:
     def __init__(self, id_name, uid, image_url, left_boundingbox, top_boundingbox, right_boundingbox, bottom_boundingbox, score, curation):
         self.id_name = id_name
         self.uid = uid
@@ -26,10 +26,10 @@ class lfwInfo:
         self.curation = curation
         pass
 
-class lfwLoader(Dataset):
+class vggfaceLoader(Dataset):
     def __init__(self, root, split="train"):
         file_list = os.listdir(root)
-        self.m_lfwInfos = []
+        self.m_vggfaceInfos = []
         for filename in file_list:
             file = open(os.path.join(root, filename))
             for line in file.readlines():
@@ -51,24 +51,26 @@ class lfwLoader(Dataset):
                 # print('score:', score)
                 # print('curation:', curation)
                 id_name = filename[:filename.rfind('.')]
-                l_lfwInfo = lfwInfo(id_name=id_name, uid=uid, image_url=image_url, left_boundingbox=left_boundingbox, top_boundingbox=top_boundingbox, right_boundingbox=right_boundingbox, bottom_boundingbox=bottom_boundingbox, score=score, curation=curation)
-                self.m_lfwInfos.append(l_lfwInfo)
+                l_vggfaceInfo = vggfaceInfo(id_name=id_name, uid=uid, image_url=image_url, left_boundingbox=left_boundingbox, top_boundingbox=top_boundingbox, right_boundingbox=right_boundingbox, bottom_boundingbox=bottom_boundingbox, score=score, curation=curation)
+                self.m_vggfaceInfos.append(l_vggfaceInfo)
 
     def __getitem__(self, index):
-        l_lfwInfo = self.m_lfwInfos[index]
-        return l_lfwInfo.id_name, l_lfwInfo.uid, l_lfwInfo.image_url, l_lfwInfo.left_boundingbox, l_lfwInfo.top_boundingbox, l_lfwInfo.right_boundingbox, l_lfwInfo.bottom_boundingbox, l_lfwInfo.score, l_lfwInfo.curation
+        l_vggfaceInfo = self.m_vggfaceInfos[index]
+        return l_vggfaceInfo.id_name, l_vggfaceInfo.uid, l_vggfaceInfo.image_url, l_vggfaceInfo.left_boundingbox, l_vggfaceInfo.top_boundingbox, l_vggfaceInfo.right_boundingbox, l_vggfaceInfo.bottom_boundingbox, l_vggfaceInfo.score, l_vggfaceInfo.curation
 
     def __len__(self):
-        return len(self.m_lfwInfos)
+        return len(self.m_vggfaceInfos)
 
 if __name__ == '__main__':
     local_path = os.path.expanduser('~/Data/face/vgg_face_dataset/files')
     local_image_path = os.path.expanduser('~/Data/face/vgg_face_dataset/images')
     batch_size = 1
-    dst = lfwLoader(local_path)
+    dst = vggfaceLoader(local_path)
     trainloader = DataLoader(dst, batch_size=batch_size)
     for i, (id_names, uids, image_urls, left_boundingboxs, top_boundingboxs, right_boundingboxs, bottom_boundingboxs, scores, curations) in enumerate(trainloader):
         print(i)
+        if i == 100000:
+            exit()
         # print(uids)
         id_names_0 = id_names[0]
         uids_0 = uids[0]
@@ -80,12 +82,11 @@ if __name__ == '__main__':
         # print(image_urls_0)
         unique_image_urls_path_0 = os.path.join(local_image_path, id_names_0, uids_0+'.jpg')
 
-        x_0 = float(left_boundingboxs_0)
-        y_0 = float(top_boundingboxs_0)
-        width_0 = abs(float(left_boundingboxs_0)-float(right_boundingboxs_0))
-        height_0 = abs(float(top_boundingboxs_0)-float(bottom_boundingboxs_0))
+        # x_0 = float(left_boundingboxs_0)
+        # y_0 = float(top_boundingboxs_0)
+        # width_0 = abs(float(left_boundingboxs_0)-float(right_boundingboxs_0))
+        # height_0 = abs(float(top_boundingboxs_0)-float(bottom_boundingboxs_0))
         # if os.path.exists(unique_image_urls_path_0):
-        #     print('exist')
         #     imgs_0 = misc.imread(unique_image_urls_path_0)
         #     ax = plt.subplot(111)
         #     ax.imshow(imgs_0)
@@ -97,5 +98,5 @@ if __name__ == '__main__':
         if not os.path.exists(image_path):
             os.mkdir(image_path)
         # if not os.path.exists(unique_image_urls_path_0):
-        os.system("wget -c {} -O {}".format(image_urls_0, unique_image_urls_path_0))
+        os.system("proxychains4 wget -c {} -O {} -t 1".format(image_urls_0, unique_image_urls_path_0))
         # exit()
